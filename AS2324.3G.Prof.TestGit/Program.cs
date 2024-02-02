8d028ecc1601a94c4ad27c3bbf4078cc4814f27a
Filippo Albertini <filippo@filippoalbertini.it>
2024-02-02 09:16:51 +0100

    Aggiungere i file di progetto.

---
+++ AS2324.3G.Prof.TestGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AS2324._3G.Prof.TestGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
